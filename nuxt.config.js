ade15b1825ab5a546a764aa93e0001b321ec30d6
conradGoh <46958188+conradGoh@users.noreply.github.com>
2020-06-08 19:18:40 +0800

    Tutorial 6

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,17 +12,15 @@ module.exports = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      { href: 'https://cdn.jsdelivr.net/npm/animate.css@3.5.1', rel:
-      'stylesheet', type:'text/css'}
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { href: 'https://cdn.jsdelivr.net/npm/animate.css@3.5.1', rel:'stylesheet', type:'text/css'}
     ],
     script:[
-      { src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js', type:
-      'text/javascript' },
-      { src: 'https://kit.fontawesome.com/<script src="https://kit.fontawesome.com/bd10c83925.js" crossorigin="anonymous"></script>.js',
-      crossorigin: 'anonymous', type:'text/javascript'},
+      { src: 'https://code.jquery.com/jquery-3.4.1.slim.min.js', type:'text/javascript' },
+      { src: 'https://kit.fontawesome.com/bd10c83925.js', crossorigin:'anonymous', type:'text/javascript'},
       ]
   },
+  
   /*
   ** Customize the progress-bar color
   */
